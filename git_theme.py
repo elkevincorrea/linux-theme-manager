9f6406b41ddca7eb0a98b8ec96bba1335b371237
Kevin Joan Correa del Castillo <kevinjoan16@gmail.com>
2018-06-22 19:27:14 -0500

    Feat identify icon theme

--- git_theme.py
+++ git_theme.py
@@ -2,15 +2,20 @@ from git import Repo
 import os
 import tempfile
 import shutil
+import click
 
 def add_from_git(git_url):
     with tempfile.TemporaryDirectory() as tmpdirname:
         dir = clone(git_url, tmpdirname)
         theme_dir = get_themes_dirs(dir)
-        install_theme(theme_dir)
+        if theme_dir != None:
+            install_theme(theme_dir)
 
-def is_theme_or_icon():
-    pass
+def is_icon(theme_dir):
+    with open(os.path.join(theme_dir, 'index.theme')) as index_theme:
+        first_line = index_theme.readline()
+        return str(first_line).find("[Icon Theme]") != -1
+            
 
 def clone(git_url, tmpdirname):
     """
@@ -18,7 +23,7 @@ def clone(git_url, tmpdirname):
     """
     repo_name = os.path.splitext(os.path.basename(git_url))[0]
     repo = Repo.clone_from(git_url, os.path.join(tmpdirname, repo_name), branch = 'master')
-    return os.path.join(os.path.dirname(repo.git_dir))
+    return os.path.dirname(repo.git_dir)
 
 def get_themes_dirs(dirname):
     theme_dir = None
@@ -26,10 +31,12 @@ def get_themes_dirs(dirname):
         theme_dir = dirname
     return theme_dir
 
-
 def install_theme(theme_dir):
-    if not os.path.exists(theme_dir):
-        if theme_dir != None:
-            shutil.move(theme_dir, os.path.join(os.environ['HOME'], '.themes/'))
+    """
+    Install a theme for the current user
+    """
+    target_dir = os.path.join(os.environ['HOME'], '.themes/' if not is_icon(theme_dir) else '.icons/')
+    if not os.path.exists(os.path.join(target_dir, os.path.basename(theme_dir))):
+        shutil.move(theme_dir, target_dir)
     else:
-        print("Existe")
+        click.secho("The theme already exists", fg="yellow")
