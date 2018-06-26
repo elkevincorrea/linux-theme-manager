60684310e1823d77eb4932fee035f16e38613242
Kevin Joan Correa del Castillo <kevinjoan16@gmail.com>
2018-06-25 22:49:56 -0500

    Feat install many themes from a repo

--- git_theme.py
+++ git_theme.py
@@ -7,8 +7,8 @@ import click
 def add_from_git(git_url):
     with tempfile.TemporaryDirectory() as tmpdirname:
         dir = clone(git_url, tmpdirname)
-        theme_dir = get_themes_dirs(dir)
-        if theme_dir != None:
+        theme_dirs = get_themes_dirs(dir)
+        for theme_dir in theme_dirs:
             install_theme(theme_dir)
 
 def is_icon(theme_dir):
@@ -26,10 +26,15 @@ def clone(git_url, tmpdirname):
     return os.path.dirname(repo.git_dir)
 
 def get_themes_dirs(dirname):
-    theme_dir = None
+    theme_dirs = []
     if os.path.exists(os.path.join(dirname, 'index.theme')):
-        theme_dir = dirname
-    return theme_dir
+        theme_dirs.append(dirname)
+    else:
+        source_dirs = next(os.walk(dirname))[1]
+        for dir in source_dirs:
+            if os.path.exists(os.path.join(dirname, dir, 'index.theme')):
+                theme_dirs.append(os.path.join(dirname, dir))
+    return theme_dirs
 
 def install_theme(theme_dir):
     """
@@ -39,4 +44,4 @@ def install_theme(theme_dir):
     if not os.path.exists(os.path.join(target_dir, os.path.basename(theme_dir))):
         shutil.move(theme_dir, target_dir)
     else:
-        click.secho("The theme already exists", fg="yellow")
+        click.secho("The theme {0} already exists".format(os.path.basename(theme_dir)), fg="yellow")
